a3c760a15e803acdf6d5eb85a78a0e8e338f9f96
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-02-21 18:42:55 -0300

    test: add unit tests for findUserByCpf

--- tests/unit/userService.test.ts
+++ tests/unit/userService.test.ts
@@ -1,7 +1,7 @@
 import userFactory from '../factories/userFactory';
 import userService from '../../src/services/userService';
 import userRepository from '../../src/repositories/userRepository';
-import { faker } from '@faker-js/faker';
+import { SendUser } from '../../src/types/userTypes';
 
 describe('Test user service methods', () => {
   it('it should throw an error for invalid CPF', async () => {
@@ -45,4 +45,36 @@ describe('Test user service methods', () => {
       expect(userRepository.addUser).toBeCalled();
     }
   );
+
+  it('it should throw an error for invalid CPF', async () => {
+    const newUser: SendUser = await userFactory.generateInvalidCpfUser();
+
+    const result = userService.findUserByCpf(newUser.cpf);
+
+    expect(result).rejects.toEqual({ type: 'unprocessable', message: 'o CPF passado é inválido'});
+  });
+
+  it('it should throw an error if the CPF is not registered', async () => {
+    const newUser: SendUser = await userFactory.generateUser();
+
+    jest
+        .spyOn(userRepository, 'findUserByCpf')
+        .mockResolvedValue(undefined);
+
+    const result = userService.findUserByCpf(newUser.cpf);
+
+    expect(result).rejects.toEqual({ type: 'not_found', message: 'Usuário não encontrado'});
+  });
+
+  it('it should throw an error if the CPF is not registered', async () => {
+    const newUser = await userFactory.generateUser();
+
+    jest
+        .spyOn(userRepository, 'findUserByCpf')
+        .mockResolvedValue({id: 1, ...newUser});
+
+    const result = await userService.findUserByCpf(newUser.cpf);
+
+    expect(result.cpf).toEqual(newUser.cpf);
+  });
 });
